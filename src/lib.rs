cd6eda35c28d65ea623b08496eee548623517864
TyOverby <ty@pre-alpha.com>
2015-03-09 13:18:09 -0700

    remove everything that I don't want to maintain

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,11 @@
-#![feature(collections, core, hash)]
-
 extern crate image;
 
 pub use packer::{Packer, GrowingPacker};
-pub use shelf_packer::ShelfPacker;
-pub use guillotine_packer::GuillotinePacker;
-pub use maxrect_packer::MaxrectPacker;
 pub use skyline_packer::SkylinePacker;
 pub use rect::Rect;
 pub use buffer2d::{Buffer2d, ResizeBuffer};
 
 mod packer;
-mod shelf_packer;
-mod guillotine_packer;
-mod maxrect_packer;
 mod skyline_packer;
 mod rect;
 mod buffer2d;
